6e4ee7b34facb698941cc9ead58158e836bd2174
Bill Tudor <bill@r11.dev>
2026-07-08 01:03:02 -0400

    Setup Boilerplate & copy Multi-Tenancy from Order-Module

---
+++ notification-module/src/main/java/com/sc07/commerce/notification/configuration/TenantPrincipal.java
@@ -0,0 +1,9 @@
+package com.sc07.commerce.notification.configuration;
+
+import java.util.UUID;
+
+public record TenantPrincipal(
+        UUID tenantId
+        //user emai address
+) {
+}
---
+++ notification-module/src/main/java/com/sc07/commerce/notification/event/OrderEventListener.java
@@ -0,0 +1,8 @@
+package com.sc07.commerce.notification.event;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderEventListener {
+
+}
